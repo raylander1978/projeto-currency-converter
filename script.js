e5ea760d76d063d4357410bc9b4e2dfcbff7ff03
raylander1978 <raylanderlaves@gmail.com>
2023-11-30 11:01:23 -0300

    dolar/euro

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
+
+    console.log(currencySelect.value)
     const dollarToday = 4.89
+    const euroToday = 5.39
 
-    const convertedValue = inputCurrencyValue / dollarToday
+    if(currencySelect.value == 'dollar'){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue/dollarToday)
+    }
+    if(currencySelect.value == 'euro'){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+    }).format(inputCurrencyValue/euroToday)
+    }
 
-    console.log(convertedValue)
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+
+
 
 }
 
